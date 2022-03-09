0eda5f9fbbeb8b9a51306da34f141d8227acc1c4
Malo FORD <malo.ford2021@campus-eni.fr>
2022-03-09 10:33:36 +0100

    fixtures finis

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,9 +3,12 @@
 namespace App\DataFixtures;
 
 use App\Entity\Campus;
+use App\Entity\Etat;
+use App\Entity\Lieu;
 use App\Entity\Participant;
+use App\Entity\Sortie;
+use App\Entity\Ville;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Generator;
@@ -24,8 +27,12 @@ class AppFixtures extends Fixture
     {
 
         $this->manager = $manager;
+        $this->addEtats();
+        $this->addVilles();
+        $this->addLieux();
         $this->addCampus();
         $this->addUsers();
+        $this->addSorties();
     }
 
     public function __construct(UserPasswordHasherInterface $passwordHasher)
@@ -44,10 +51,9 @@ class AppFixtures extends Fixture
             $user = new Participant();
             $user->setNom($this->faker->lastName);
             $user->setPrenom($this->faker->firstname);
-            $user->setTelephone("012030405");
-            $user->setRoles(array('ROLE_USER'))->setEmail($this->faker->email)
-                ->setPassword($this->hasher->hashPassword($user, '123
-                '))->setActif(true)->setCampus($this->faker->randomElement($tabCampus));
+            $user->setTelephone("0102030405");
+            $user->setRoles(array('ROLE_ADMIN'))->setEmail($this->faker->email)
+                ->setPassword($this->hasher->hashPassword($user, '123'))->setActif(true)->setCampus($this->faker->randomElement($tabCampus));
             $this->manager->persist($user);
         }
 
@@ -63,9 +69,81 @@ class AppFixtures extends Fixture
 
         for ($i = 0; $i < 4; $i++) {
             $campus = new Campus();
-            $campus->setNom($this->faker->city);
+            $campus->setNom($this->faker->region);
             $this->manager->persist($campus);
         }
         $this->manager->flush();
     }
+
+    public function addVilles()
+    {
+
+        for ($i = 0; $i < 3; $i++) {
+            $ville = new Ville();
+            $ville->setNom($this->faker->city);
+            $ville->setCodePostal($this->faker->departmentNumber);
+            $this->manager->persist($ville);
+        }
+        $this->manager->flush();
+    }
+
+    public function addLieux(){
+        $tabVilles = $this->manager->getRepository(Ville::class)->findAll();
+
+        for ($i = 0; $i < 7; $i++) {
+            $lieu = new Lieu();
+            $lieu->setNom($this->faker->city);
+            $lieu->setRue($this->faker->streetName);
+            $lieu->setVille($this->faker->randomElement($tabVilles));
+            $this->manager->persist($lieu);
+        }
+        $this->manager->flush();
+    }
+
+    public function addEtats() {
+        $etat = new Etat();
+        $etat->setLibelle("Crée");
+        $this->manager->persist($etat);
+        $etat = new Etat();
+        $etat->setLibelle("Ouverte");
+        $this->manager->persist($etat);
+        $etat = new Etat();
+        $etat->setLibelle("Cloturée");
+        $this->manager->persist($etat);
+        $etat = new Etat();
+        $etat->setLibelle("Activité en cours");
+        $this->manager->persist($etat);
+        $etat = new Etat();
+        $etat->setLibelle("Passée");
+        $this->manager->persist($etat);
+        $etat = new Etat();
+        $etat->setLibelle("Annulée");
+        $this->manager->persist($etat);
+        
+        $this->manager->flush();
+    }
+
+    public function addSorties(){
+        $tabLieux = $this->manager->getRepository(Lieu::class)->findAll();
+        $tabEtats = $this->manager->getRepository(Etat::class)->findAll();
+        $tabCampus = $this->manager->getRepository(Campus::class)->findAll();
+        $tabParticipants = $this->manager->getRepository(Participant::class)->findAll();
+
+        for ($i = 0; $i < 7; $i++) {
+            $sortie = new Sortie();
+            $sortie->setNom($this->faker->word);
+            $sortie->setDateHeureDebut($this->faker->dateTime($max = 'now', $timezone = null));
+            $sortie->setDuree($this->faker->numberBetween($min = 0, $max = 120));
+            $sortie->setDateLimiteInscription($this->faker->dateTime($max = 'now', $timezone = null));
+            $sortie->setNbInscriptionsMax($this->faker->numberBetween($min = 0, $max = 40));
+            $sortie->setInfosSortie($this->faker->sentence($nbWords = 10, $variableNbWords = true));
+            $sortie->setLieu($this->faker->randomElement($tabLieux));
+            $sortie->setEtat($this->faker->randomElement($tabEtats));
+            $sortie->setCampus($this->faker->randomElement($tabCampus));
+            $sortie->setOrganisateur($this->faker->randomElement($tabParticipants));
+            $this->manager->persist($sortie);
+        }
+        $this->manager->flush();
+    }
+
 }
